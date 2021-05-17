3d284fa152fb2fd6fc4995d9446aba0e885e541e
Batalan Vlad <batabtro@gmail.com>
2021-05-17 20:27:01 +0300

    users.json and login

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
+const session = require('express-session');
+const fs = require('fs');
+const url = require('url');
+
 
 const app = express();
 
@@ -16,13 +20,78 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
+// utilizare variabile session
+app.use(session({
+    secret:'keyboard cat',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge: null
+    }})
+);
 
-// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
-// proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
-// proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
+
+// the middleware to set the user script in the left
+function userloggedMiddleware(req, res, next){
+    console.log("Userlogged Middleware");
+    if(!req.session.views){
+        req.session.views = {};
+    }
+    if(req.session.views['userlogged'])
+    {
+        res.locals['userlogged'] = req.session.views['userlogged'];
+    }
+
+    next();
+}
+app.use(userloggedMiddleware);
+
 app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.get('/chat', (req, res) => {
+    res.render("chat");
+});
+
+app.get('/login', (req, res) => {
+    // delogare - delete user session if exists
+    var errorParam = url.parse(req.url, true).query;
+    if(req.session.views){
+        delete req.session.views['userlogged'];
+    }
+
+    var error = "";
+    if(errorParam['error'] && errorParam['error'] == "wrong-credentials")
+    {
+        error = "Nume utilizator sau parola invalide!";
+    }
+
+    res.render("login", {"error": error});
+});
+
+let allUsers = JSON.parse(fs.readFileSync("users.json"));
+app.post('/process-login', (req, res) => {
+    let userlogged = null;
+    allUsers.forEach(user => {
+        if(req.body['username'] == user.username && req.body['password'] == user.password){
+            delete user['password'];
+            userlogged = user;
+            return;
+        }
+    });
+    if(userlogged != null){
+        // set the userlogged session
+        if(!req.session.views){
+            req.session.views = {};
+        }
+        req.session.views['userlogged'] = userlogged;
+
+        res.redirect("/");
+    }
+    else{
+        res.redirect("/login?error=wrong-credentials");
+    }
+});
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
